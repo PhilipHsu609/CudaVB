d47afe7c1cd3e4909c5363bb841d3d82ad6e648e
PhilipHsu609 <mpyhacct118113@gmail.com>
2023-10-13 18:01:06 +0800

    Add histogram equalization.

---
+++ CudaDLL/hist_eq.cu
@@ -0,0 +1,158 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "helper_cuda.h"
+#include "my_cuda_lib.h"
+#include "cooperative_groups.h"
+#include "utils.h"
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+#define HISTOGRAM_SIZE 256
+
+namespace cg = cooperative_groups;
+
+extern "C" void equalizeHistCPU(const uint8_t *src, uint8_t *dst, int channels, int width, int height);
+extern "C" void equalizeHistCuda(const uint8_t *src, uint8_t *dst, int channels, int width, int height);
+
+__global__ void calculateHist(const uint8_t *src, int *hist, int channels, int width, int height);
+__global__ void calculateHistSum(const int *hist, int *histSum, int channels);
+__global__ void equalizeHist(const uint8_t *src, uint8_t *dst, const int *histSum, int channels, int width, int height);
+
+__global__ void calculateHist(const uint8_t *src, int *hist, int channels, int width, int height) {	
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	cg::thread_block g = cg::this_thread_block();
+
+	if (x >= width * height)
+		return;
+
+	extern __shared__ unsigned int histShared[]; // HISTOGRAM_SIZE * channels
+
+	if(threadIdx.x < HISTOGRAM_SIZE) {
+		for (int c = 0; c < channels; c++) {
+			histShared[threadIdx.x * channels + c] = 0;
+		}
+	}
+
+	g.sync();
+
+	for (int c = 0; c < channels; c++) {
+		atomicAdd(&histShared[int(src[x * channels + c]) * channels + c], 1);
+	}
+
+	g.sync();
+
+	if(threadIdx.x < HISTOGRAM_SIZE) {
+		for (int c = 0; c < channels; c++) {
+			atomicAdd(&hist[threadIdx.x * channels + c], histShared[threadIdx.x * channels + c]);
+		}
+	}
+}
+
+__global__ void calculateHistSum(const int *hist, int *histSum, int channels) {
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	cg::thread_block g = cg::this_thread_block();
+
+	extern __shared__ unsigned int histShared[]; // HISTOGRAM_SIZE * channels
+
+	for (int c = 0; c < channels; c++) {
+		histShared[x * channels + c] = hist[x * channels + c];
+	}
+
+	g.sync();
+
+	// Kogge-Stone Scan
+	for (int stride = 1; stride < HISTOGRAM_SIZE; stride *= 2) {
+		g.sync();
+		if (threadIdx.x >= stride) {
+			for (int c = 0; c < channels; c++) {
+				histShared[threadIdx.x * channels + c] += histShared[(threadIdx.x - stride) * channels + c];
+			}
+		}
+	}
+
+	g.sync();
+
+	for (int c = 0; c < channels; c++) {
+		histSum[x * channels + c] = histShared[x * channels + c];
+	}
+}
+
+__global__ void equalizeHist(const uint8_t *src, uint8_t *dst, const int *histSum, int channels, int width, int height) {
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	cg::thread_block g = cg::this_thread_block();
+
+	if (x >= width * height)
+		return;
+
+	extern __shared__ unsigned int histShared[]; // HISTOGRAM_SIZE * channels
+
+	if (threadIdx.x < HISTOGRAM_SIZE) {
+		for (int c = 0; c < channels; c++) {
+			histShared[threadIdx.x * channels + c] = histSum[threadIdx.x * channels + c];
+		}
+	}
+
+	g.sync();
+
+	for (int c = 0; c < channels; c++) {
+		dst[x * channels + c] = (uint8_t)lroundf(histShared[src[x * channels + c] * channels + c] * 255 / float(width * height));
+	}
+}
+
+void equalizeHistCPU(const uint8_t *src, uint8_t *dst, int channels, int width, int height) {
+	std::vector<int> hist(HISTOGRAM_SIZE * channels, 0);
+	std::vector<int> histSum(HISTOGRAM_SIZE * channels, 0);
+
+	// calculate histogram
+	for (int y = 0; y < height; y++) {
+		const uint8_t *srcRow = src + y * width * channels;
+		for (int x = 0; x < width; x++) {
+			for (int c = 0; c < channels; c++) {
+				hist[srcRow[x * channels + c] * channels + c]++;
+			}
+		}
+	}
+
+	// calculate cumulative histogram
+	histSum[0] = hist[0];
+	for (int y = 1; y < HISTOGRAM_SIZE; y++) {
+		for (int c = 0; c < channels; c++) {
+			histSum[y * channels + c] = hist[y * channels + c] + histSum[(y - 1) * channels + c];
+		}
+	}
+
+	// equalize histogram
+	for (int y = 0; y < height; y++) {
+		const uint8_t *srcRow = src + y * width * channels;
+		uint8_t *distRow = dst + y * width * channels;
+		for (int x = 0; x < width; x++) {
+			for (int c = 0; c < channels; c++) {
+				distRow[x * channels + c] = (uint8_t)std::round(histSum[srcRow[x * channels + c] * channels + c] * 255 / float(width * height));
+			}
+		}
+	}
+}
+
+void equalizeHistCuda(const uint8_t *src, uint8_t *dst, int channels, int width, int height) {
+	// Reference:
+	//     https://github.com/nuwandda/cuda-histogram-equalization/blob/main/kernel.cu
+
+	int *devHist, *devHistSum{};
+	int size = HISTOGRAM_SIZE * channels * sizeof(int);
+
+	checkCudaErrors(cudaMalloc(&devHist, size));
+	checkCudaErrors(cudaMalloc(&devHistSum, size));
+
+	dim3 threadsPerBlock(HISTOGRAM_SIZE * 4);
+	dim3 numBlocks(divUp(width * height, threadsPerBlock.x));
+	calculateHist<<<numBlocks, threadsPerBlock, size>>>(src, devHist, channels, width, height);
+
+	calculateHistSum<<<1, HISTOGRAM_SIZE, size>>>(devHist, devHistSum, channels);
+
+	equalizeHist<<<numBlocks, threadsPerBlock, size>>>(src, dst, devHistSum, channels, width, height);
+
+	cudaFree(devHist);
+	cudaFree(devHistSum);
+}
--- CudaDLL/my_cuda_lib.h
+++ CudaDLL/my_cuda_lib.h
@@ -3,15 +3,22 @@
 
 #pragma once
 
-#include <cstdint>
-
 #define DLL_EXPORT __declspec(dllexport)
+
+#ifndef __CUDACC__
+#define __CUDACC__
+#endif
+
+#include <cstdint>
 
 /*
 	* Get the number of CUDA devices
 */
 extern "C" DLL_EXPORT int deviceCount();
 
+/*
+	* GPU memory management
+*/
 extern "C" DLL_EXPORT bool cudaAlloc(void **gpuPtr, size_t size);
 extern "C" DLL_EXPORT bool cudaRelease(void **gpuPtr);
 extern "C" DLL_EXPORT bool toGPU(void *cpuPtr, void *gpuPtr, size_t size);
@@ -42,7 +49,7 @@ extern "C" DLL_EXPORT void rgb2hsvCpu(const uint8_t *src, uint8_t *dst, int widt
 extern "C" DLL_EXPORT void rgb2hsvCuda(const uint8_t *src, uint8_t *dst, int width, int height);
 
 /*
-    * Convolution
+	* Convolution
 	* 
 	* Note: Kernel size must be odd
 */
@@ -50,28 +57,42 @@ extern "C" DLL_EXPORT void conv2DCpu(const uint8_t *src, uint8_t *dst, int chann
 extern "C" DLL_EXPORT void conv2DCuda(const uint8_t *src, uint8_t *dst, int channels, int width, int height, const float *kernel, int kernelSize);
 
 /*
-    * Dilation
-    *
-    * Note: Kernel is a 0/1 square matrix
+	* Dilation
+	*
+	* Note: Kernel is a 0/1 square matrix
 */
 extern "C" DLL_EXPORT void dilateCpu(const uint8_t * src, uint8_t * dst, int channels, int width, int height, const int *kernel, int kernelSize);
 extern "C" DLL_EXPORT void dilateCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height, const int *kernel, int kernelSize);
 
 /*
-    * Erosion
-    *
-    * Note: Kernel is a 0/1 square matrix
+	* Erosion
+	*
+	* Note: Kernel is a 0/1 square matrix
 */
 extern "C" DLL_EXPORT void erodeCpu(const uint8_t * src, uint8_t * dst, int channels, int width, int height, const int *kernel, int kernelSize);
 extern "C" DLL_EXPORT void erodeCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height, const int *kernel, int kernelSize);
 
+/*
+	* Gaussian pyramid
+*/
 extern "C" DLL_EXPORT void pyrUpCpu(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
-extern "C" DLL_EXPORT void pyrUpCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
+//extern "C" DLL_EXPORT void pyrUpCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
 
 extern "C" DLL_EXPORT void pyrDownCpu(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
-extern "C" DLL_EXPORT void pyrDownCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
+//extern "C" DLL_EXPORT void pyrDownCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
 
+/*
+	* Resize image
+	*
+	* TODO: Implement bilinear interpolation with CUDA texture memory
+*/
 extern "C" DLL_EXPORT void bilinearCpu(const uint8_t * src, uint8_t * dst, int channels, int width, int height, int dstWidth, int dstHeight);
 extern "C" DLL_EXPORT void bilinearCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height, int dstWidth, int dstHeight);
+
+/*
+	* Histogram equalization
+*/
+extern "C" DLL_EXPORT void equalizeHistCPU(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
+extern "C" DLL_EXPORT void equalizeHistCuda(const uint8_t * src, uint8_t * dst, int channels, int width, int height);
 
 #endif // !MY_CUDA_LIB_H
